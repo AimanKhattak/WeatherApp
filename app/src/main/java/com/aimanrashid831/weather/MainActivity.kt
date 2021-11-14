102f3e3adbe92409fc01e491248d953959edcd65
AimanKhattak <aimanrashid831@gmail.com>
2021-11-14 16:44:23 +0500

    Commit landscape view of app

--- app/src/main/java/com/aimanrashid831/weather/MainActivity.kt
+++ app/src/main/java/com/aimanrashid831/weather/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.aimanrashid831.weather
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var linearButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linearButton = findViewById(R.id.linearButton)
+        linearButton.setOnClickListener{
+            val intent = Intent(this, WeatherLinearLayout::class.java)
+            startActivity(intent)
+        }
     }
 }
